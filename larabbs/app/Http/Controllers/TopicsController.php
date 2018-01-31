55005a8681906eea53306c4c7e97581b1011ca59
vagrant <vagrant@homestead>
2018-01-31 06:28:36 +0000

    User can create topic

--- larabbs/app/Http/Controllers/TopicsController.php
+++ larabbs/app/Http/Controllers/TopicsController.php
@@ -6,6 +6,8 @@ use App\Models\Topic;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
+use App\Models\Category;
+use Auth;
 
 class TopicsController extends Controller
 {
@@ -27,13 +29,8 @@ class TopicsController extends Controller
 
 	public function create(Topic $topic)
 	{
-		return view('topics.create_and_edit', compact('topic'));
-	}
-
-	public function store(TopicRequest $request)
-	{
-		$topic = Topic::create($request->all());
-		return redirect()->route('topics.show', $topic->id)->with('message', 'Created successfully.');
+        $categories = Category::all();
+        return view('topics.create_and_edit', compact('topic', 'categories'));
 	}
 
 	public function edit(Topic $topic)
@@ -57,4 +54,12 @@ class TopicsController extends Controller
 
 		return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
 	}
+    public function store(TopicRequest $request, Topic $topic)
+   {
+       $topic->fill($request->all());
+       $topic->user_id = Auth::id();
+       $topic->save();
+
+       return redirect()->route('topics.show', $topic->id)->with('message', 'Created successfully.');
+   }
 }
